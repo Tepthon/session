df040317ca3a9145d7f2b455ad3487829bace06d
Tepthon <106397883+Tepthon@users.noreply.github.com>
2022-06-24 23:50:46 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    API_ID = int(os.environ.get('API_ID', 0))
+    API_ID = int(os.environ.get('API_ID',0))
     API_HASH = str(os.environ.get('API_HASH', None))
     BOT_TOKEN = str(os.environ.get('BOT_TOKEN', None))
     MONGO_URI = str(os.environ.get('MONGO_URI', None))
